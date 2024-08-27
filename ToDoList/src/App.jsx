3b2bec212d6290143a26211b29bde07eeb743d79
Aniket Ingole <aniketingolenet@gmail.com>
2024-08-27 20:24:03 +0530

    first commit

---
+++ ToDoList/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './components/Home'
+import Create from './components/Create'
+
+function App() {
+
+  return (  
+     <div>
+      <Home />
+     </div>  
+  )
+}
+
+export default App;
